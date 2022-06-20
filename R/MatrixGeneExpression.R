18643bfcc192e4145803e69ff76b97d7bcf534b5
Andy Chen <yhc@sh03-06n38.int>
2022-06-19 23:01:40 -0700

    add ADT

--- R/MatrixGeneExpression.R
+++ R/MatrixGeneExpression.R
@@ -34,9 +34,10 @@ addGeneExpressionMatrix <- function(
   parallelParam = NULL,
   strictMatch = FALSE,
   force = TRUE,
+  isADT=FALSE,
   logFile = createLogFile("addGeneExpressionMatrix")
-  ){
-
+){
+  
   .validInput(input = input, name = "input", valid = c("ArchRProj", "character"))
   .validInput(input = seRNA, name = "seRNA", valid = c("SummarizedExperiment"))
   .validInput(input = chromSizes, name = "chromSizes", valid = c("granges"))
@@ -48,8 +49,8 @@ addGeneExpressionMatrix <- function(
   .validInput(input = strictMatch, name = "strictMatch", valid = c("boolean"))
   .validInput(input = force, name = "force", valid = c("boolean"))
   .validInput(input = logFile, name = "logFile", valid = c("character"))
-
-
+  
+  
   if(inherits(input, "ArchRProject")){
     ArrowFiles <- getArrowFiles(input)
     allCells <- rownames(getCellColData(input))
@@ -70,18 +71,18 @@ addGeneExpressionMatrix <- function(
   if(!all(file.exists(ArrowFiles))){
     stop("Error Input Arrow Files do not all exist!")
   }
-
+  
   .startLogging(logFile = logFile)
   .logThis(mget(names(formals()),sys.frame(sys.nframe())), "addGeneExpressionMatrix Input-Parameters", logFile = logFile)
-
+  
   cellsInArrows <- unlist(lapply(ArrowFiles, .availableCells), use.names=FALSE)
   if(!is.null(allCells)){
     cellsInArrows <- allCells
   }
-
+  
   overlap <- sum(cellsInArrows %in% colnames(seRNA)) / length(cellsInArrows)
   .logMessage("Overlap w/ scATAC = ", round(overlap,3), logFile = logFile, verbose = TRUE)
-
+  
   if(overlap == 0){
     stop("No overlapping cell names found between ArrowFiles and seRNA object! Cell names in ArrowFiles must match colnames in seRNA!")
   } else if(overlap != 1) {
@@ -91,11 +92,11 @@ addGeneExpressionMatrix <- function(
       .logMessage("Warning! Not all cells in input exist in seRNA! This may cause downstream issues with functions that require information from all cells. For example, addIterativeLSI() will not work on this GeneExpressionMatrix!", logFile = logFile, verbose = TRUE)
     }
   }
-
+  
   splitCells <- split(cellsInArrows, stringr::str_split(cellsInArrows, pattern = "#", simplify=TRUE)[,1])
   overlapPerSample <- unlist(lapply(splitCells, function(x) sum(x %in% colnames(seRNA))))
   .logMessage("Overlap Per Sample w/ scATAC : ", paste(paste(names(overlapPerSample), round(overlapPerSample,3), sep = "="), collapse=","), logFile = logFile, verbose = TRUE)
-
+  
   #Get QC Info
   assay(seRNA) <- Matrix::Matrix(assay(seRNA), sparse=TRUE)
   nUMI <- Matrix::colSums(assay(seRNA))
@@ -111,7 +112,7 @@ addGeneExpressionMatrix <- function(
   #Filter seRNA
   seRNA <- seRNA[BiocGenerics::which(seqnames(seRNA) %bcin% seqnames(chromSizes))]
   seRNA <- seRNA[BiocGenerics::which(seqnames(seRNA) %bcni% excludeChr)]
-
+  
   #Dedup
   idxDup <- which(rownames(seRNA) %in% rownames(seRNA[duplicated(rownames(seRNA))]))
   names(idxDup) <- rownames(seRNA)[idxDup]
@@ -120,21 +121,21 @@ addGeneExpressionMatrix <- function(
     dupOrder <- dupOrder[!duplicated(names(dupOrder))]
     seRNA <- seRNA[-as.vector(idxDup[idxDup %ni% dupOrder])]
   }
-
+  
   #Add Index To RNA Ranges
   features <- rowRanges(seRNA)
   features <- sort(sortSeqlevels(features), ignore.strand = TRUE)
   features <- split(features, seqnames(features))
   features <- lapply(features, function(x){
-  mcols(x)$idx <- seq_along(x)
+    mcols(x)$idx <- seq_along(x)
     return(x)
   })
   features <- Reduce("c",features)
   rowData(seRNA)$idx <- features[rownames(seRNA)]$idx
-
+  
   .logThis(qcInfo, "qcInfo", logFile = logFile)
-
-   #Add args to list
+  
+  #Add args to list
   args <- mget(names(formals()), sys.frame(sys.nframe()))#as.list(match.call())
   args$ArrowFiles <- ArrowFiles
   args$allCells <- allCells
@@ -143,41 +144,41 @@ addGeneExpressionMatrix <- function(
   args$registryDir <- file.path(outDir, "addGeneExpressionMatRegistry")
   args$qcInfo <- qcInfo
   args$seRNA <- seRNA
-
+  
   #Remove Input from args
   args$input <- NULL
   args$chromSizes <- NULL
   args$strictMatch <- NULL
-
+  
   #Run With Parallel or lapply
   outList <- .batchlapply(args)
-
-    .endLogging(logFile = logFile)
-
-    #Return Output
-    if(inherits(input, "ArchRProject")){
-
-      qcInfo <- qcInfo[rownames(qcInfo) %in% input$cellNames, ]
-
-      for(i in seq_len(ncol(qcInfo))){
-    input <- addCellColData(
-      ArchRProj = input, 
-      data = as.vector(qcInfo[,i]), 
-      name = paste0(colnames(qcInfo)[i]), 
-      cells = paste0(rownames(qcInfo)), 
-      force = force
-    )
-      }
+  
+  .endLogging(logFile = logFile)
+  
+  #Return Output
+  if(inherits(input, "ArchRProject")){
     
-      return(input)
-
-    }else{
-
-      return(unlist(outList))
-
+    qcInfo <- qcInfo[rownames(qcInfo) %in% input$cellNames, ]
+    
+    for(i in seq_len(ncol(qcInfo))){
+      input <- addCellColData(
+        ArchRProj = input, 
+        data = as.vector(qcInfo[,i]), 
+        name = paste0(colnames(qcInfo)[i]), 
+        cells = paste0(rownames(qcInfo)), 
+        force = force
+      )
     }
-
-
+    
+    return(input)
+    
+  }else{
+    
+    return(unlist(outList))
+    
+  }
+  
+  
 } 
 
 .addGeneExpressionMat <- function(
@@ -194,8 +195,8 @@ addGeneExpressionMatrix <- function(
   force = FALSE,
   verbose = TRUE,
   logFile = NULL
-  ){
-
+){
+  
   ArrowFile <- ArrowFiles[i]
   sampleName <- .sampleName(ArrowFile)
   
@@ -207,38 +208,45 @@ addGeneExpressionMatrix <- function(
   if(is.null(tstart)){
     tstart <- Sys.time()
   }
- 
+  
   #Get all cell ids before constructing matrix
   if(is.null(cellNames)){
     cellNames <- .availableCells(ArrowFile)
   }
-
+  
   if(!is.null(allCells)){
     cellNames <- cellNames[cellNames %in% allCells]
   }
-
+  
   #Identify Overlapping Cells
   cellNames <- cellNames[cellNames %in% colnames(seRNA)]
   seRNA <- seRNA[, cellNames]
-
-  dfParams <- data.frame(
-    scaleTo = scaleTo, 
-    exclude = excludeChr,
+  
+  if(isADT == FALSE){
+    dfParams <- data.frame(
+      scaleTo = scaleTo, 
+      exclude = excludeChr,
+      stringsAsFactors = FALSE
+    )
+  }else{
+    dfParams <- data.frame( 
+      exclude = excludeChr,
+      stringsAsFactors = FALSE
+    )
+  }
+  
+  featureDF <- data.frame(
+    seqnames = paste0(seqnames(seRNA)), 
+    idx = mcols(seRNA)$idx, 
+    start = start(seRNA), 
+    end = end(seRNA), 
+    name = rownames(seRNA),
+    strand = as.integer(strand(seRNA)),
     stringsAsFactors = FALSE
   )
-
-  featureDF <- data.frame(
-  seqnames = paste0(seqnames(seRNA)), 
-  idx = mcols(seRNA)$idx, 
-  start = start(seRNA), 
-  end = end(seRNA), 
-  name = rownames(seRNA),
-    strand = as.integer(strand(seRNA)),
-  stringsAsFactors = FALSE
-  )
-
+  
   .logThis(featureDF, "featureDF", logFile = logFile)
-
+  
   ######################################
   # Initialize SP Mat Group
   ######################################
@@ -252,24 +260,27 @@ addGeneExpressionMatrix <- function(
     featureDF = featureDF,
     force = TRUE
   )
-
+  
   ######################################
   # Normalize and Insert Log2 Normalized Counts
   ######################################
-
-  assay(seRNA) <- .normalizeCols(assay(seRNA), scaleTo = scaleTo)
-
+  if(isADT == FALSE){
+    assay(seRNA) <- .normalizeCols(assay(seRNA), scaleTo = scaleTo)
+  }else{
+    assay(seRNA) <- clr(assay(seRNA))
+  }
+  
   uniqueChr <- unique(featureDF$seqnames)
-
+  
   for(z in seq_along(uniqueChr)){
-
-  o <- tryCatch({
-
-    o <- h5closeAll()
-    chr <- uniqueChr[z]
-    matz <- assay(seRNA[BiocGenerics::which(seqnames(seRNA)==chr), ])
-    .logDiffTime(sprintf("Adding %s to %s for Chr (%s of %s)!", sampleName, matrixName, z, length(uniqueChr)), tstart, verbose = verbose, logFile = logFile)
-
+    
+    o <- tryCatch({
+      
+      o <- h5closeAll()
+      chr <- uniqueChr[z]
+      matz <- assay(seRNA[BiocGenerics::which(seqnames(seRNA)==chr), ])
+      .logDiffTime(sprintf("Adding %s to %s for Chr (%s of %s)!", sampleName, matrixName, z, length(uniqueChr)), tstart, verbose = verbose, logFile = logFile)
+      
       #Write sparseMatrix to Arrow File!
       o <- .addMatToArrow(
         mat = matz, 
@@ -281,45 +292,50 @@ addGeneExpressionMatrix <- function(
         addRowVarsLog2 = TRUE #add for integration analyses
       )
       gc()
-
+      
       if(z %% 3 == 0 | z == length(uniqueChr)){
         gc()
       }
-
+      
     }, error = function(e){
-
+      
       errorList <- list(
         ArrowFile = ArrowFile,
         chr = chr,
         mat = if(exists("matz", inherits = FALSE)) matz else "matz"
       )
-
+      
       .logError(e, fn = ".addGeneExpressionMat AddToArrow", info = sampleName, errorList = errorList, logFile = logFile)
-
+      
     })
-
-
+    
+    
   }
-
+  
   #Add Info To Arrow Files
   allCells <- .availableCells(ArrowFile, passQC = FALSE)
-
+  
   qcInfoi <- qcInfo[rownames(qcInfo) %in% colnames(seRNA), ]
-
+  
   for(i in seq_len(ncol(qcInfo))){
-
+    
     infoi <- rep(-1, length(allCells))
     names(infoi) <- allCells
     infoi[rownames(qcInfoi)] <- qcInfoi[,i]
-
+    
     o <- h5closeAll()
     h5write(infoi, file = ArrowFile, paste0("Metadata/", colnames(qcInfoi)[i]))
     o <- h5closeAll()
-
+    
   }
-
+  
   ArrowFile
-
+  
 }
 
+clr <- function(data) t(
+  apply(data, 1, function(x) log1p(
+    x / (exp(sum(log1p(x[x > 0]), na.rm = TRUE) / length(x)))
+  ))
+)
 
